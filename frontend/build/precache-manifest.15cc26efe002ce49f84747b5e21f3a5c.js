53bc5d2ed6c72c5f11ff71fbf33380f6398e236a
prince <pibewiro@yahoo.com>
2020-01-28 21:19:19 -0300

    updates

---
+++ frontend/build/precache-manifest.15cc26efe002ce49f84747b5e21f3a5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00cdca397b9b24576145cec65a494113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b59499cf821ac13128",
+    "url": "/static/css/main.71667838.chunk.css"
+  },
+  {
+    "revision": "3e42bc0a721beb3bbd02",
+    "url": "/static/js/2.6557acb4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6557acb4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4b59499cf821ac13128",
+    "url": "/static/js/main.f1692b37.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  },
+  {
+    "revision": "7b1d746e03ac95c946f336440882d7c9",
+    "url": "/static/media/login.7b1d746e.jpeg"
+  },
+  {
+    "revision": "aa8f45f9f49a35fe5d69ba3249f93b13",
+    "url": "/static/media/trucksSC.aa8f45f9.jpg"
+  }
+]);
